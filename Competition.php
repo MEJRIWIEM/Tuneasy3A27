aca788cecc1397ef374d86c36be8e34f6831ff48
mayakammoun <58006708+mayakammoun@users.noreply.github.com>
2021-03-04 10:59:16 +0100

    Add files via upload

---
+++ Competition.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompetitionRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=CompetitionRepository::class)
+ */
+class Competition
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank(message="le nom est vide")
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     *  @Assert\NotBlank(message="description est vide")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *  @Assert\NotBlank(message="Categorie est vide")
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     *  @Assert\NotBlank(message="nombre de place est vide")
+     */
+    private $nombre_de_place;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     *  @Assert\NotBlank(message="image est vide")
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *  @Assert\NotBlank(message="adresse est vide")
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     *  @Assert\NotBlank(message="la date est vide")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(?string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getNombreDePlace(): ?int
+    {
+        return $this->nombre_de_place;
+    }
+
+    public function setNombreDePlace(?int $nombre_de_place): self
+    {
+        $this->nombre_de_place = $nombre_de_place;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(?string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
